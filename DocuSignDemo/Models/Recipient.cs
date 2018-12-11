92c86ab0b71f75f8855c96fe0f79d5114721af78
Pratik Adagale <Pratik.Adagale@ey.com>
2018-12-11 20:35:19 +0530

    Demo App

---
+++ DocuSignDemo/Models/Recipient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace DocusignDemo.Models
+{
+    public partial class Recipient
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Email
+        {
+            get;
+            set;
+        }
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
